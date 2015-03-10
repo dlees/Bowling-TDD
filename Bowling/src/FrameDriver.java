a0e0d3624fa81cf646a54b89ba89890cc4c98189
dlees <dlees@umich.edu>
2015-03-09 23:57:57 -0700

    score calc on prev passes
    Easy. Again, works only if you call it in order.

--- Bowling/src/FrameDriver.java
+++ Bowling/src/FrameDriver.java
@@ -2,21 +2,22 @@
 public class FrameDriver {
 	private BowlingDisplay display;
 	
-	private int pinsHitRoll1 = 0;
+	private int score = 0;
 	
 	public FrameDriver(BowlingDisplay display) {
 		this.display = display;
 	}
 
 	public void performRoll1(int frameNum, int pinsHit) {
-		pinsHitRoll1 = pinsHit;
+		score += pinsHit;
 		display.setRoll1(frameNum, pinsHit);
 	}
 
-	public void performRoll2(int frameNum, int pinsHit) {
+	public void performRoll2(int frameNum, int pinsHit) {		
+		score += pinsHit;
+		
 		display.setRoll2(frameNum, pinsHit);
-		display.setScore(frameNum, pinsHitRoll1 + pinsHit);	
-		pinsHitRoll1 = 0;
+		display.setScore(frameNum, score);	
 	}
 
 }
