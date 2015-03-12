f2e2f08a2518cfec75d12a6af3fcfe308740c6d7
dlees <dlees@umich.edu>
2015-03-11 22:40:30 -0700

    Spare score passes
    But it's ugly. Should I've refactored now? Meh, it works.

--- Bowling/src/FrameDriver.java
+++ Bowling/src/FrameDriver.java
@@ -3,6 +3,7 @@ public class FrameDriver {
 	private BowlingDisplay display;
 	
 	private int score = 0;
+	private boolean last_was_spare = false;
 	
 	public FrameDriver(BowlingDisplay display) {
 		this.display = display;
@@ -11,6 +12,12 @@ public class FrameDriver {
 	public void performRoll1(int frameNum, int pinsHit) {
 		score += pinsHit;
 		display.setRoll1(frameNum, pinsHit);
+		
+		if (last_was_spare) {
+			display.setScore(frameNum-1, score);
+			score += pinsHit;
+		}
+		
 	}
 
 	public void performRoll2(int frameNum, int pinsHit) {		
@@ -18,6 +25,7 @@ public class FrameDriver {
 		
 		if (score == 10) {
 			display.setMark(frameNum, BowlingDisplay.SPARE);
+			last_was_spare = true;
 		}
 		
 		display.setRoll2(frameNum, pinsHit);
